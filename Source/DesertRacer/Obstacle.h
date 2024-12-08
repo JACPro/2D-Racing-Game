6b056acae994ce1dd09231e8b8487e0eeca7558c
JCunningham <jamesacpro@gmail.com>
2024-12-08 21:24:53 +0000

    Create obstacle class with capsule and sprite components
    Signed-off-by: JCunningham <jamesacpro@gmail.com>

---
+++ Source/DesertRacer/Obstacle.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/CapsuleComponent.h"
+#include "PaperSpriteComponent.h"
+
+#include "Obstacle.generated.h"
+
+UCLASS()
+class DESERTRACER_API AObstacle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UPaperSpriteComponent* ObstacleSprite;
+
+	AObstacle();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+};
