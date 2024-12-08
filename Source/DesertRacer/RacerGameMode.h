cfe1d078e480d9954995c52b11ff9fdb8b4886b1
JCunningham <jamesacpro@gmail.com>
2024-12-08 21:24:18 +0000

    Add new game mode and set it as default game mode
    Signed-off-by: JCunningham <jamesacpro@gmail.com>

---
+++ Source/DesertRacer/RacerGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RacerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESERTRACER_API ARacerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
